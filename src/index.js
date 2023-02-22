54047226bbee105380004d00de03002764bd3c92
Palienko Mihai <palienko@advantum.pro>
2023-02-22 20:50:00 +0300

    [feat] header []

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App.js';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import Orders from './pages/Orders';
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+    },
+    {
+        path: '/create-order',
+        element: <Orders />,
+    },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
-root.render(
-    <App />
-);
-
-
+root.render(<RouterProvider router={router} />);
