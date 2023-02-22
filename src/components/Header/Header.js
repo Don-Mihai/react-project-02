512135a9e1e2032b117757e37cb9d09c0e006cbb
Palienko Mihai <palienko@advantum.pro>
2023-02-22 20:07:23 +0300

    [feat] react router []

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
         // todo: вёрстка хэдэра
         <header className='header'>
             Header
+            <a href='http://localhost:3000/home'> home </a>
+            <a href='http://localhost:3000/order'>order</a>
         </header>
     )
 }
